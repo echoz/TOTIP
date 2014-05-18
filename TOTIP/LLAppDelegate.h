2e7cb050a397d7075e32918f8136b6e70ef996a3
Jeremy Foo <jeremyfoo@gmail.com>
2014-05-18 18:54:11 -0400

    Initial Commit

---
+++ TOTIP/LLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLAppDelegate.h
+//  TOTIP
+//
+//  Created by Jeremy Foo on 18/5/14.
+//  Copyright (c) 2014 LazyLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TOTIP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TOTIP
+//
+//  Created by Jeremy Foo on 18/5/14.
+//  Copyright (c) 2014 LazyLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LLAppDelegate class]));
+    }
+}
