f2db40a2d8315821563a37ffb1006e701fececf4
Jeremy Foo <jeremyfoo@gmail.com>
2014-05-20 02:22:34 -0400

    Scaffold for entry detail view

---
+++ Entry Detail/TOTIPDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TOTIPDetailViewController.h
+//  TOTIP
+//
+//  Created by Jeremy Foo on 20/5/14.
+//  Copyright (c) 2014 LazyLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOTIPDetailViewController : UIViewController
+
+@end
