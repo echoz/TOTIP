5c9b22b53f4e5c64851fdd0fecf245f45edf00b9
Jeremy Foo <jeremyfoo@gmail.com>
2014-05-21 02:51:57 -0400

    Initial Entry View

---
+++ Entry Detail/TOTIPEntryView.h
@@ -0,0 +1,21 @@
+//
+//  TOTIPEntryView.h
+//  TOTIP
+//
+//  Created by Jeremy Foo on 21/5/14.
+//  Copyright (c) 2014 LazyLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOTIPEntryView : UIView
+
+@property (nonatomic, weak, readonly) UIImageView *imageView;
+
+@property (nonatomic, weak, readonly) UILabel *titleLabel;
+@property (nonatomic, weak, readonly) UILabel *byLineLabel;
+
+@property (nonatomic, weak, readonly) UILabel *summaryLabel;
+@property (nonatomic, weak, readonly) UILabel *categoryLabel;
+
+@end
---
+++ Entry Detail/TOTIPEntryView.m
@@ -0,0 +1,31 @@
+//
+//  TOTIPEntryView.m
+//  TOTIP
+//
+//  Created by Jeremy Foo on 21/5/14.
+//  Copyright (c) 2014 LazyLabs. All rights reserved.
+//
+
+#import "TOTIPEntryView.h"
+
+@implementation TOTIPEntryView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
