30fb1d4da721db0c57e78d5541ed9c8a57adbcc0
Jeremy Foo <jeremyfoo@gmail.com>
2014-05-21 00:58:58 -0400

    Add skeleton for initial collection view cell

---
+++ Entries List/TOTIPEntryCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TOTIPEntryCollectionViewCell.h
+//  TOTIP
+//
+//  Created by Jeremy Foo on 21/5/14.
+//  Copyright (c) 2014 LazyLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOTIPEntryCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Entries List/TOTIPEntryCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  TOTIPEntryCollectionViewCell.m
+//  TOTIP
+//
+//  Created by Jeremy Foo on 21/5/14.
+//  Copyright (c) 2014 LazyLabs. All rights reserved.
+//
+
+#import "TOTIPEntryCollectionViewCell.h"
+
+@implementation TOTIPEntryCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
